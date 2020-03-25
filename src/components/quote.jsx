6dac5c1c59c95903f6319b776244df56517970a5
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-25 13:52:53 -0400

    fetched and displayed random joke from api

--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -1,15 +1,31 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Quote extends Component {
-    state = {
-        quote: "Chuck is a force."
+  state = {
+    quote: 'Hiding from Norris...'
+  };
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(
+        `https://api.chucknorris.io/jokes/random?category=animal`
+      );
+      const data = await res.json();
+      this.setState({
+        quote: data.value
+      });
+    } catch (error) {
+      this.setState({
+        quote: error.message
+      });
     }
-  
-    render() {
-     const { quote } = this.state;
+  }
+
+  render() {
+    const { quote } = this.state;
     return (
       <div>
-        <p id="chuckSays">{quote}</p>
+        <p id='chuckSays'>{quote}</p>
       </div>
     );
   }
